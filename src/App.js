7a1755c5d15d4d10ccc86cfe087fe21e22d3a013
TharushiDithya <97993879+TharushiDithya@users.noreply.github.com>
2022-06-07 20:39:05 +0530

    update colors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import {Input } from "antd";
+import { InputNumber, Button, Progress } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+  function onChange(value) {
+    console.log("changed", value);
+  } 
+
+function App(){
+    return(
+  
+    <div >
+  
+          <div><h2 className="txt"> Your current avaliable points:</h2></div>
+          <div className = "ProgressB">
+      <Progress
+      type="circle"
+      strokeColor={{
+        '0%': '#108ee9',
+        '100%': '#87d068',
+        
+      }}
+      width="250px"
+      percent={80}
+      
+      /></div>
+      
+      
+       <div>
+    
+       <h2 className="txt"> Offer points:</h2>
+       <InputNumber min={1} max={10} onChange={onChange} style={{marginTop:"50px", marginLeft:"100px"}} /> 
+      
+        </div>
+
+      
+       <h2 className="txt" name="Shoutout"> Shoutout:</h2>
+
+<Input.TextArea
+              style={{
+                width: "50%",
+                marginLeft:"150px",
+                height:"100px",
+              }}
+            />
+      
+
+
+       <space >
+          <Button type="primary" htmlType="submit" className="btn" >
+            Submit
+          </Button>
+        </space>
+        
     </div>
-  );
+
+      
+
+       
+        
+    );
 }
 
 export default App;
+
